4f27734628557eb6bae7f189316b6f93eb24eabb
Tiffany Chandra <tiffanychandra@g.ucla.edu>
2020-02-17 15:34:04 -0800

    tiffany's frontend first changes

--- frontend/gamelinks/config-overrides.js
+++ frontend/gamelinks/config-overrides.js
@@ -1,6 +1,5 @@
 const { override, fixBabelImports, addLessLoader } = require("customize-cra");
 module.exports = override(
-  // do stuff with the webpack config...
   fixBabelImports("import", {
     libraryName: "antd",
     libraryDirectory: "es",
