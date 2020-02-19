112a2242abecba7e7173b36545c6834503dbf4af
jasche7 <jasonc1172@gmail.com>
2020-02-18 19:56:11 -0800

    Last commit broke everything, undoing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import { Link, BrowserRouter as Router } from 'react-router-dom';
-import { Typography, Menu, Input, Icon, Dropdown } from 'antd';
+import { Menu, Input, Icon } from 'antd';
 
 import 'antd/dist/antd.css'
 
 const { Search } = Input;
 
-const pmenu = (
-    <Menu>
-      <Menu.Item>
-        <Link to="/hosts">hosts</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/people">friends</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/profile">profile</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/messages">inbox</Link>
-      </Menu.Item>
-    </Menu>
-  );
 
 class Header extends React.Component {
     state = {
@@ -39,28 +23,11 @@ class Header extends React.Component {
         const profileImage = this.props.user ? <Icon type='user' /> : <Icon type="user" /> // When we add backend, this should have the profile picture instead.
         return (
             <Router>
-                <Menu 
-                    align='right' 
-                    theme='dark'
-                    style={{ lineHeight: '64px' }}
-                    onClick={this.handleClick} 
-                    selectedKeys={ [this.state.selected] } 
-                    mode='horizontal'>
-                    <Typography style={{ float: 'left', color: 'white' }} level={1}>
-                        GAMELINKS
-                    </Typography>
-                    <Menu.Item key='home'>
-                        <Link to='/'>
-                            <Icon type='home' />
-                        </Link>
-                    </Menu.Item>
+                <Menu onClick={this.handleClick} selectedKeys={ [this.state.selected] } mode='horizontal'>
                     <Menu.Item key='people'>
-                        <Dropdown overlay={pmenu}>
-                            <a className='ant-dropdown-link' onClick={e => e.preventDefault()}>
-                                people
-                                <Icon type='down' />
-                            </a>
-                        </Dropdown>
+                        <Link to='/people'>
+                            people
+                        </Link>
                     </Menu.Item>
                     <Menu.Item key='games'>
                         <Link to='/games'>
@@ -73,7 +40,7 @@ class Header extends React.Component {
                         </Link>
                     </Menu.Item>
                     { profileImage }
-                    <Search placeholder='search' onSearch={value => console.log(value)} style={{ width: 200 }} />
+                    <Search placeholder='search' style={{ width: 200 }} />
                 </Menu>
             </Router>
         );
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,37 +1,12 @@
 import React from 'react';
-import { Layout, Button } from 'antd';
-import { BrowserRouter as Router } from 'react-router-dom';
-import FacebookLoginButton from "./FacebookLoginButton";
 
-const { Content } = Layout;
-
-const responseFacebook = (response) => {
-  console.log(response);
-}
-
-function onFacebookLogin(loginStatus, resultObject){
-  // if (loginStatus === true) {
-  //   this.setState({
-  //     username: resultObject.user.name
-  //   });
-  // } else {
-  //   alert('Facebook login error');
-  // }
-}
 
 class Home extends React.Component {
     render() {
         return (
-            <Router>
-                <Content style={{ padding: "0 50px", marginTop: 64 }}>
-                    <div align="center">
-                        <FacebookLoginButton onLogin={onFacebookLogin}>
-                            <Button>Login with Facebook</Button>
-                        </FacebookLoginButton>
-                    </div>
-                </Content>
-            </Router>
-        )
+            <div></div>
+        );
     };
 }
+
 export default Home;
