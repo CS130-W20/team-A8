0e560c9a03096304353bca341b3ccff538e44424
Tiffany Chandra <tiffanychandra@g.ucla.edu>
2020-03-08 00:31:53 -0800

    messaging + hosts page changes

--- frontend/gamelinks/src/components/Hosts.js
+++ frontend/gamelinks/src/components/Hosts.js
@@ -30,7 +30,6 @@ class Hosts extends React.Component {
     this.prevHosting = this.prevHosting.bind(this);
     this.nextFavorites = this.nextFavorites.bind(this);
     this.prevFavorites = this.prevFavorites.bind(this);
-    this.changeProfilePic = this.changeProfilePic.bind(this);
     const values = queryString.parse(props.location.search);
     this.state.userId = values.id;
     this.carouselHosting = React.createRef();
@@ -63,15 +62,66 @@ class Hosts extends React.Component {
         }
       })
       .then(() => {
-        this.getGameList("hosting");
+        this.getHostList(userInfo.data.city);
       })
+      // .then(() => {
+      //   this.getGameList("hosting");
+      // })
       .catch(err => {
         console.log(err);
       });
   }
+  nextHosting() {
+    this.carouselHosting.next();
+  }
+
+  prevHosting() {
+    this.carouselHosting.prev();
+  }
+
+  getHostList = async loc => {
+    // once google maps api is set up we can post people near user location
+    if (!this.state.userInfo || !this.state.userInfo[type]) {
+      // TEST:  if (!this.state.userInfo || !this.state.userInfo[type])
+      return <Empty />;
+    }
+    const idList = this.state.userInfo[loc];
+    const HostPromises = [];
+    for (let i = 0; i < idList.length; i += 1) {
+      const hostReq = axios.get(
+        `${config.backend_url}/profile/getProfileUserInformation?id=${idList[i]}`
+      );
+      HostPromises.push(hostReq);
+    }
+    const hostInfo = await Promise.all(HostPromises);
+    this.setState({ hosts: hostInfo });
+  };
+
+  getGameList = async type => {
+    if (!this.state.userInfo || !this.state.userInfo[type]) {
+      // TEST:  if (!this.state.userInfo || !this.state.userInfo[type])
+      return <Empty />;
+    }
+    const idList = this.state.userInfo[type];
+    const InfoPromises = [];
+    for (let i = 0; i < idList.length; i += 1) {
+      const gameReq = axios.get(
+        `${config.backend_url}/igdb/game?id=${idList[i]}`
+      );
+      const coverReq = axios.get(
+        `${config.backend_url}/igdb/cover?id=${idList[i]}`
+      );
+      InfoPromises.push(gameReq, coverReq);
+    }
+    const gameInfo = await Promise.all(InfoPromises);
+    type === "hosting"
+      ? this.setState({ hosting: gameInfo })
+      : this.setState({ favorites: gameInfo });
+  };
+
   render() {
-    let createCards = type => {
-      console.log("creatingCards");
+    let createGameCards = type => {
+      console.log("creatingGameCards");
       const gameInfo = this.state[type];
       if (!gameInfo) return;
       const cards = [];
@@ -101,7 +151,24 @@ class Hosts extends React.Component {
       }
       return cards;
     };
-    const hostingCards = createCards("hosting") || []; // NOT TEST
+    let createHostCards = loc => {
+      console.log("creatingHostCards");
+      const hostInfo = this.state[loc];
+      if (!hostInfo) return;
+      const cards = [];
+      for (let i = 0; i < hostInfo.length; i += 2) {
+        cards.push(
+          <div className="card">
+            <Link to={`/profile/?id=${hostInfo[i].data.id}`}>
+              <img src={`/profile/?id=${hostInfo[i].data.profilePicture}`} />
+            </Link>
+          </div>
+        );
+      }
+      return cards;
+    };
+    const hostCards = createHostCards("hosts") || [];
+    const gameCards = createGameCards("hosting") || []; // NOT TEST
     const props = {
       dots: true,
       speed: 500,
@@ -109,27 +176,42 @@ class Hosts extends React.Component {
       slidesToScroll: 4
     };
     return (
-      <div class="main">
-        <Title>Hosts For You</Title>
-        <Carousel
-          class="carousel"
-          ref={node => (this.carousel = node)}
-          {...props}
-        >
-          {/* render host cards */}
-        </Carousel>
-        <Avatar
-          className="arrow-left"
-          size="large"
-          icon="left-circle"
-          onClick={this.previous}
-        />
-        <Avatar
-          className="arrow-right"
-          size="large"
-          icon="right-circle"
-          onClick={this.next}
-        />
+      // clicking on a card links to profile.js
+      <div>
+        <Title level={2}>Hosts for You</Title>
+        <hr />
+        <div>
+          <div>
+            {hostCards.length ? (
+              <>
+                <div>
+                  <Carousel
+                    ref={node => (this.carouselHosting = node)}
+                    {...props}
+                    slidesToShow={Math.min(4, hostCards.length)}
+                  >
+                    {hostCards}
+                  </Carousel>
+                </div>
+                <br />
+                <div className="scroll-group">
+                  <Icon
+                    type="left-circle"
+                    className="scroll"
+                    onClick={this.prevHosting}
+                  />
+                  <Icon
+                    type="right-circle"
+                    className="scroll"
+                    onClick={this.nextHosting}
+                  />
+                </div>
+              </>
+            ) : (
+              <Empty />
+            )}
+          </div>
+        </div>
       </div>
     );
   }
