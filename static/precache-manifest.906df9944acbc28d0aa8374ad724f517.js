c63b83b5a26bc59d13992b9c359d24a2c31d4822
jasche7 <jasonc1172@gmail.com>
2020-03-13 19:35:29 -0700

    Added static files

---
+++ static/precache-manifest.906df9944acbc28d0aa8374ad724f517.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650ce6068e828dc0b6d3e4d4650c81fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "043200a5098eb10a96a6",
+    "url": "/static/css/2.3378788b.chunk.css"
+  },
+  {
+    "revision": "55c9ee28e831c49a49fe",
+    "url": "/static/css/main.a654a52e.chunk.css"
+  },
+  {
+    "revision": "043200a5098eb10a96a6",
+    "url": "/static/js/2.91898a3f.chunk.js"
+  },
+  {
+    "revision": "fea492374587e4893f20915c1fb5c692",
+    "url": "/static/js/2.91898a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c9ee28e831c49a49fe",
+    "url": "/static/js/main.6b6c155c.chunk.js"
+  },
+  {
+    "revision": "52a104cb7d839cc0c4ca",
+    "url": "/static/js/runtime-main.938e1a6c.js"
+  }
+]);
